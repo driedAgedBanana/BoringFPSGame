eceaa1080be88912194e25710da903644c087f20
driedAgedBanana <b.cao@sintlucasedu.nl>
2024-06-13 22:42:03 +0200

    I dont know why or how, but things just work okay?
    This is based on hope, dream and miracle.

--- BoringFPSGame/Assets/scripts/CameraController.cs
+++ BoringFPSGame/Assets/scripts/CameraController.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float SenX;
-    [SerializeField] private float SenY;
+    [Header("References")]
+    [SerializeField] WallRun wallRun;
 
-    [SerializeField] Transform camera;
-    [SerializeField] private Transform orientation;
+    [SerializeField] private float sensX = 100f;
+    [SerializeField] private float sensY = 100f;
 
-    private float mouseX;
-    private float mouseY;
+    [SerializeField] Transform cam = null;
+    [SerializeField] Transform orientation = null;
 
-    private float CameraMultiplier = 0.01f;
+    float mouseX;
+    float mouseY;
 
-    private float xRotation;
-    private float yRotation;
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
 
     private void Start()
     {
@@ -26,20 +29,15 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        CameraInput();
-
-        camera.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.transform.localRotation = Quaternion.Euler(0, yRotation, 0);
-    }
-
-    private void CameraInput()
-    {
         mouseX = Input.GetAxisRaw("Mouse X");
         mouseY = Input.GetAxisRaw("Mouse Y");
 
-        yRotation += mouseX * SenX * CameraMultiplier;
-        xRotation -= mouseY * SenY * CameraMultiplier;
+        yRotation += mouseX * sensX * multiplier;
+        xRotation -= mouseY * sensY * multiplier;
 
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, wallRun.tilt);
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
--- BoringFPSGame/Assets/scripts/PlayerMovementScript.cs
+++ BoringFPSGame/Assets/scripts/PlayerMovementScript.cs
@@ -4,98 +4,105 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    private float playerHeight = 2;
+    float playerHeight = 2f;
 
-    [SerializeField] private Transform orientation;
-
-    [Header("Keybinds")]
-    public KeyCode JumpKey = KeyCode.Space;
-    public KeyCode SprintKey = KeyCode.LeftShift;
+    [SerializeField] Transform orientation;
 
     [Header("Movement")]
-    public float moveSpeed = 6f;
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float airMultiplier = 0.4f;
     float movementMultiplier = 10f;
-    [SerializeField] float AirMultiplier = 0.4f;
-
-    private float HorizontalMovement;
-    private float VerticalMovement;
 
     [Header("Sprinting")]
-    [SerializeField] private float walkSpeed = 4f;
-    [SerializeField] private float sprintSpeed = 7f;
-    [SerializeField] private float acceleration = 10f;
+    [SerializeField] float walkSpeed = 4f;
+    [SerializeField] float sprintSpeed = 6f;
+    [SerializeField] float acceleration = 10f;
 
-    [Header("Jumping and ground modifier")]
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private Transform groundCheck;
-    public float gravityModifier;
-    private bool isGrounded;
-    private float groundDistance = 0.4f;
-    public float JumpForce;
+    [Header("Jumping")]
+    public float jumpForce = 5f;
 
-    private RaycastHit slopeHit;
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
 
     [Header("Drag")]
-    [SerializeField] private float groundDrag = 6f;
-    [SerializeField] private float airDrag = 2f;
+    [SerializeField] float groundDrag = 6f;
+    [SerializeField] float airDrag = 2f;
 
-    private Vector3 MoveDirection;
-    private Vector3 SlopeMoveDirection;
+    float horizontalMovement;
+    float verticalMovement;
 
-    public Rigidbody rb;
-    public Transform cameraTransform;
+    [Header("Ground Detection")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float groundDistance = 0.2f;
+    public bool isGrounded { get; private set; }
 
-    // Start is called before the first frame update
-    void Start()
+    Vector3 moveDirection;
+    Vector3 slopeMoveDirection;
+
+    Rigidbody rb;
+
+    RaycastHit slopeHit;
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-        Physics.gravity *= gravityModifier;
-
-        // Reference to the camera's transform
-        cameraTransform = Camera.main.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //check the player's bottom
-        print(isGrounded);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        MovementInput();
+        MyInput();
         ControlDrag();
         ControlSpeed();
 
-        if (Input.GetKeyDown(JumpKey) && isGrounded)
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
         {
-            PlayerJump();
+            Jump();
         }
 
-        SlopeMoveDirection = Vector3.ProjectOnPlane(MoveDirection, slopeHit.normal);
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
     }
 
-    private void MovementInput()
+    void MyInput()
     {
-        HorizontalMovement = Input.GetAxisRaw("Horizontal");
-        VerticalMovement = Input.GetAxisRaw("Vertical");
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
 
-        // Calculate movement direction based on camera's orientation
-        Vector3 forward = cameraTransform.forward;
-        Vector3 right = cameraTransform.right;
-
-        // Project forward and right vectors onto the XZ plane (ignore Y component)
-        forward.y = 0f;
-        right.y = 0f;
-
-        forward.Normalize();
-        right.Normalize();
-
-        MoveDirection = (orientation.forward * VerticalMovement + orientation.right * HorizontalMovement).normalized;
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
     }
 
-    private void ControlSpeed()
+    void Jump()
     {
-        if (Input.GetKey(SprintKey) && isGrounded)
+        if (isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void ControlSpeed()
+    {
+        if (Input.GetKey(sprintKey) && isGrounded)
         {
             moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
         }
@@ -105,13 +112,7 @@ public class PlayerMovementScript : MonoBehaviour
         }
     }
 
-    private void PlayerJump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, 0 , rb.velocity.z);
-        rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
-    }
-
-    private void ControlDrag()
+    void ControlDrag()
     {
         if (isGrounded)
         {
@@ -125,40 +126,22 @@ public class PlayerMovementScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        MovingPlayer();
+        MovePlayer();
     }
 
-    private void MovingPlayer()
+    void MovePlayer()
     {
         if (isGrounded && !OnSlope())
         {
-            rb.AddForce(MoveDirection * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
         }
         else if (isGrounded && OnSlope())
         {
-            rb.AddForce(SlopeMoveDirection * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
         }
         else if (!isGrounded)
         {
-            rb.AddForce(MoveDirection * moveSpeed * movementMultiplier * AirMultiplier, ForceMode.Acceleration);
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
         }
-    }
-
-    private bool OnSlope()
-    {
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
-        {
-            if (slopeHit.normal != Vector3.up)
-            {
-                //if this is true, we are on a slope
-                return true;
-            }
-            else
-            {
-                //or else we are on the flat ground
-                return false;
-            }
-        }
-        return false;
     }
 }

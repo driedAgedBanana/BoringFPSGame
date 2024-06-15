7b2e297a099257cb0bd3aef130a7e7e6b69c8008
driedAgedBanana <b.cao@sintlucasedu.nl>
2024-06-15 20:47:27 +0200

    Adding shooting mechanics

--- BoringFPSGame/Assets/_scripts/shootingScript.cs
+++ BoringFPSGame/Assets/_scripts/shootingScript.cs
@@ -4,57 +4,21 @@ using UnityEngine;
 
 public class shootingScript : MonoBehaviour
 {
-    public KeyCode shootingKey = KeyCode.Mouse0;
-    public int gunDamage = 1;
-    public float fireRate = 0.25f;
-    public float weaponRange = 50f;
-    public float hitForce = 100f;
-    public Transform gunEnd;
+    public KeyCode shootKey = KeyCode.Mouse0;
+    public Transform shootingPoint;
+    public GameObject bullet;
+    public float Power;
+    [SerializeField] private float bulletTimeAlive = 5f;
 
-    private Camera cam;
-    private WaitForSeconds shotDuration = new WaitForSeconds(0.75f);
+    [SerializeField] private Camera mainCamera;
 
-    private LineRenderer shootingLine;
-    private float nextFireRound;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        shootingLine = GetComponent<LineRenderer>();
-        cam = GetComponentInParent<Camera>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(shootingKey) && Time.time > nextFireRound)
+        if (Input.GetKeyDown(shootKey))
         {
-            nextFireRound = Time.time + fireRate;
-            StartCoroutine(shootingMoment());
-
-            Vector3 raycastOrigin = cam.ViewportToWorldPoint(new Vector3(.5f, .5f, 0.0f));
-
-            RaycastHit hit;
-
-            shootingLine.SetPosition(0, gunEnd.position);
-
-            if (Physics.Raycast(raycastOrigin, cam.transform.forward, out hit, weaponRange))
-            {
-                shootingLine.SetPosition(1, hit.point);
-            }
-            else
-            {
-                shootingLine.SetPosition(1, cam.transform.forward * weaponRange);
-            }
+            GameObject bulletItSelf = Instantiate(bullet, shootingPoint.position, transform.rotation);
+            bulletItSelf.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.right) * Power, ForceMode.VelocityChange);
+            Destroy(bulletItSelf, bulletTimeAlive);
         }
-    }
-
-    private IEnumerator shootingMoment()
-    {
-        shootingLine.enabled = true;
-
-        yield return shotDuration;
-
-        shootingLine.enabled = false;
     }
 }

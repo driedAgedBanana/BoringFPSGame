355ff37c6e75dadd4ddbff719878f359304d641e
driedAgedBanana <b.cao@sintlucasedu.nl>
2024-06-22 21:49:32 +0200

    update common

--- BoringFPSGame/Assets/_scripts/Teleportation.cs
+++ BoringFPSGame/Assets/_scripts/Teleportation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Teleportation : MonoBehaviour
 {
+    public GameObject playerObj;
     public GameObject teleporationDestination;
     // Start is called before the first frame update
     void Start()
@@ -19,12 +20,21 @@ public class Teleportation : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //playerItSelf.transform.position = teleporationDestination.transform.position;
-        //Debug.Log("Player touched the teleport pad!");
-
-        if (other.CompareTag("Player"))
+        if (other.gameObject == playerObj)
         {
+            if (teleporationDestination != null)
+            {
+                Debug.Log("Current Player Position: " + playerObj.transform.position);
+                Debug.Log("Teleportation Destination Position: " + teleporationDestination.transform.position);
 
+                playerObj.transform.position = teleporationDestination.transform.position;
+
+                Debug.Log("Player teleported to: " + playerObj.transform.position);
+            }
+            else
+            {
+                Debug.LogError("Teleportation destination is not assigned.");
+            }
         }
     }
 }
